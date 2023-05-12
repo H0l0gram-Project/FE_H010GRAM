f24d6080dd8b23f945210f7b460a00336e487a2d
v-va <36254544+v-va@users.noreply.github.com>
2023-05-12 21:21:23 +0900

    #1 Init : 초기 설정 (#8)
    * Fix : readme.md
    
    * init : 폴더 구조 및 초기 설정

---
+++ src/pages/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import GlobalStyle from "../components/GlobalStyle"
+import Button from "../components/Button";
+
+const Main = () => {
+    return (
+        <>
+            <GlobalStyle />
+            <Button />
+        </>
+    );
+};
+
+export default Main;
