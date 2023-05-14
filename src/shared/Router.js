4b6eab4cd396ffd5a62131bccd9d796a30dde212
v-va <poruch0915@gmail.com>
2023-05-14 23:04:53 +0900

    Design : 레이아웃(header, footer, nav, layout)

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Main from '../pages/Main'
+import Profile from '../pages/Profile'
+import Post from '../pages/Post'
+import Details from '../pages/Details'
+import Layout from '../components/Layout/Layout'
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Main />}/>
+        <Route path='/' element={<Layout><Main /></Layout>}/>
+        <Route path='/profile' element={<Layout><Profile /></Layout>}/>
+        <Route path='/post' element={<Layout><Post /></Layout>}/>
+        <Route path='/details/1' element={<Layout><Details /></Layout>}/>
       </Routes>
     </BrowserRouter>
   )
