fe96b61a7695e7f6522b0f0ef290e6eadf33fa98
v-va <36254544+v-va@users.noreply.github.com>
2023-05-18 18:40:16 +0900

    #1 Fix : 로고 수정 (#41)
    * Fix : 로고 수정
    
    * Fix : 로고 수정
    
    * Fix : 로고 수정

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -70,7 +70,7 @@ const Signup = () => {
         <SignUpContainer>
             <InnerBox>
                 <TitleBox>
-                    <img src={process.env.PUBLIC_URL + "/H0l0GRAM_logo.png"} alt="홀로그램 로고" />
+                    <img src={'https://slack-imgs.com/?c=1&o1=ro&url=https%3A%2F%2Flh3.googleusercontent.com%2FzFWk4zcnlsrjZlzIfe4NCa8odPTvugQKO6PRafjIyMHkWgxUttDafSUe-GUtSs9ptSr5iodnUaIGxIJL8LjMYJXZVcCUfYDFbSX-t2MeVUhrdC9Jfw_TOr-bVei63hTyRnGJNgIhN9samnn8Q__f8nU7Tm1XJif7FTn8WkzhkIsBUaTLFW2TC4TXBJnW8GbuGxenRQy7g_1X1pRPaKEHlG8shem1jrglP6tWGi27ovQO9DD0mUhyDWZE7tlW4bagi1K7INiG0k36EOUfTHPIPkld_uQR82rRB_vcN-CEc9fy7QiwJdjtLHVSeqBGgSaUjb94FAEwBdDDPwYkYl6b28rubBz-x-2nNEfBqVWdL87RfrTdeUpcC83H3uIE7zCjVGzdaKYbFcl2tjcZTg942PZKMjaMytPBOH-1O0Ek4S-snkGjxryFltm1Er8sadnURn0YAMwtGeZ_lOnF_Pq10ne04iBB1kIwsklw-oy-5u7Fn6jZDpS726WSH7TRZ1S334V-_LAFnPesQcnL8jS2KBS2yrcXsLOYnp1jhTHYP6Z7Ddqo4KJXIGl-9a2FUbswFgq8ODu0UAuegZ89TVeM9lt1DpYS74szIdZmWtBn6X0KTU26xJdbX7iB5GxMmRpec8hOaNhsP6Vzlz030NOW4qUm5AUMTOE6_hQ7isC_aU2p53_Qi8H16awIeDf86IUQLz3xv4RwDlofHlsm-5bRd-L8ZMZb6qjcxCLW_6br3qd9WOU0anKjx02c5TLjRqSH1oRYmoxg88vUBosVYvykrKsaTUfhqvLRtPAuHAJ4ErWRlwBtgBQL_bzHGeIl7b3N_XvYSfgp6OznaltghZXfuJv6mCd7oP6Iortu6AWevOvf4G6uanGKi_rjjU_-JZYIzGOivevRlaqlWiuxWAy9GB8kPt_AZvv1p_NmXuGwEMN9'} alt="홀로그램 로고" />
                 </TitleBox>
                 <form onSubmit={onSubmitHandler}>
                     <div className="emailBox">
